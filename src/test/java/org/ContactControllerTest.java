9c9a715249b693643cf42b3674dd27000b43fe00
FlavioKde <flaviodavirro@gmail.com>
2025-08-24 01:57:25 +0200

    Feat ContactControllerTest, refactor ContactServiceTest

---
+++ src/test/java/org/ContactControllerTest.java
@@ -0,0 +1,81 @@
+package org;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.propydis.studio.controller.ContactController;
+import com.propydis.studio.dto.mysql.ContactCreateDTO;
+import com.propydis.studio.dto.mysql.mapper.ContactMapper;
+import com.propydis.studio.model.mysql.Contact;
+import com.propydis.studio.model.mysql.ContactStatus;
+import com.propydis.studio.service.mysql.ContactService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ContactControllerTest {
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private ContactService contactService;
+
+    @InjectMocks
+    private ContactController contactController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(contactController).build();
+        objectMapper = new ObjectMapper();
+
+    }
+
+    @Test
+    public void testCreateContact() throws Exception {
+        ContactCreateDTO contactCreateDTO = new ContactCreateDTO();
+
+        contactCreateDTO.setFirstName("FirstName");
+        contactCreateDTO.setLastName("LastName");
+        contactCreateDTO.setEmail("Email");
+        contactCreateDTO.setPhone("Phone");
+        contactCreateDTO.setMessage("Message");
+
+
+        Contact  contact = ContactMapper.toEntity(contactCreateDTO);
+
+        contact.setId(1L);
+        contact.setReplyMessage("Message_Reply");
+        contact.setCreatedAt(LocalDateTime.now());
+        contact.setStatus(ContactStatus.NEW);
+
+        Mockito.when(contactService.save(Mockito.any()))
+                .thenReturn(contact);
+
+        mockMvc.perform(post("/api/contact/save")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(contactCreateDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.firstname").value("FirstName"))
+                .andExpect(jsonPath("$.lastname").value("LastName"))
+                .andExpect(jsonPath("$.email").value("Email"))
+                .andExpect(jsonPath("$.phone").value("Phone"))
+                .andExpect(jsonPath("$.message").value("Message"))
+                .andExpect(jsonPath("$.replyMessage").value("Message_Reply"))
+                .andExpect(jsonPath("$.contactStatus").value("NEW"));
+
+    }
+}
